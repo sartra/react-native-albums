d88d97f2d0fbb53b8d51ed4bed38062f8ad0cf4d
Sarah Renshaw <srenshaw@nycl137457m.us.dci.discovery.com>
2018-07-18 18:53:15 -0400

    begin native app

---
+++ index.ios.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native'; 
+
+const App = () => (
+  <Text>Blah</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, View } from 'react-native'; 
+
+
+const Header = () => {
+  const { textStyle, viewStyle } = styles; 
+ 
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  ); 
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    height: 60,
+    padding: 15,
+    shadowColor: '#000', 
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20,
+    color: 'red'
+  }
+}; 
+
+export default Header; 
